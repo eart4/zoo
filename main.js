b395039eb223dfafb2474d7292447d4544bfd06a
eart4 <metrika987@gmail.com>
2022-11-27 15:44:31 +0300

    added burger menu

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function (){
+    let burger = document.getElementById('burger');
+
+
+    burger.addEventListener('click', function (){
+
+        burger.classList.toggle('open');
+    })
+
+    document.querySelector('.burger').addEventListener('click', _=> {
+        document.querySelector('.nav-burger').classList.toggle('nav-open');
+      })
 
 });
+
+
+

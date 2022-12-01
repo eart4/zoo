0cea0e375d3f871147fb2bfb5c973ab27ba459a7
eart4 <metrika987@gmail.com>
2022-12-01 21:19:29 +0300

    fixed slider

--- main.js
+++ main.js
@@ -50,6 +50,60 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
+  let myTopAnimals = new Swiper(".myAnimals", {
+    slidesPerView: 3,
+
+    spaceBetween: 31,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints:{
+        1221:{
+            slidesPerView: 3,
+        },
+
+        700:{
+            slidesPerView: 2,
+        },
+    },
+
+      freeMode: true,
+  });
+
+
+  let myLowAnimals = new Swiper(".myAnimals-1", {
+    slidesPerView: 3,
+
+    spaceBetween: 31,
+    freeMode: true,
+
+    breakpoints:{
+
+        320:{
+            slidesPerView: 1,
+        },
+
+
+        
+        
+
+        780:{
+            slidesPerView: 2,
+        },
+        1221:{
+            slidesPerView: 3,
+        },
+
+
+    }
+  });
+
+  myTopAnimals.controller.control = myLowAnimals;
+  myLowAnimals.controller.control = myTopAnimals;
+
 
 
 
